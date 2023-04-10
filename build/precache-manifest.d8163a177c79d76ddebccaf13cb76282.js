224ea4abbc7f45137bfca1bf980febfd352f634b
Jake-Oz <jakeandrach@me.com>
2023-04-10 12:00:48 +1000

    Including Build files

---
+++ build/precache-manifest.d8163a177c79d76ddebccaf13cb76282.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "58f2b38dceef19a4a4d2",
+    "url": "/static/css/main.99098d12.chunk.css"
+  },
+  {
+    "revision": "58f2b38dceef19a4a4d2",
+    "url": "/static/js/main.5815d8bd.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "f3afda117f246c896c6c",
+    "url": "/static/js/2.ca48165b.chunk.js"
+  },
+  {
+    "revision": "f058bd337a84ec462e61e1c53438fc0f",
+    "url": "/index.html"
+  }
+];
